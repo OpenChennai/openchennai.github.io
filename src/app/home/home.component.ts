0289223eb19bcee7cb113471f8fa1462bfdf6691
Anirudh S <anirudh24seven@gmail.com>
2019-01-08 08:44:39 +0530

    Add Events in Chennai Google Calendar into the page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public dateString: String;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  getDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    let month = date.getMonth();
+    let monthString = month.toString(10);
+    month = month + 1;
+    if (month < 10) {
+      monthString = '0' + monthString;
+    }
+    const day = date.getDate();
+    let dayString = day.toString(10);
+    if (day < 10) {
+      dayString = '0' + dayString;
+    }
+    this.dateString = year + monthString + dayString;
   }
 
 }
