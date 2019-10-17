54f888f31d0680c09106597037d250fe912773e1
Anirudh S <anirudh24seven@gmail.com>
2019-10-17 10:00:43 +0530

    Upgrade version, fix issues and remove unnecessary features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 const routes: Routes = [
-  // { path: '/leaderboard', component: LeaderboardComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faFacebook, faGithub, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
-import { IssueFormComponent } from './issue-form/issue-form.component';
 import { FormsModule } from '@angular/forms';
-import { MapViewComponent } from './map-view/map-view.component';
-import { NguiMapModule } from './ngui-map';
-import {} from '@types/googlemaps';
 
 library.add(faTwitter);
 library.add(faFacebook);
@@ -24,10 +19,7 @@ library.add(faGithub);
 @NgModule({
   declarations: [
     AppComponent,
-    LeaderboardComponent,
     HomeComponent,
-    IssueFormComponent,
-    MapViewComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +28,6 @@ library.add(faGithub);
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyB5-F7KikHZOoT322H_Dam1X6Gs7MNyWhM'})
   ],
   providers: [],
   bootstrap: [AppComponent]
